4ef28d574591109cf08719861c705ed701075c51
Kostu96 <kostu96@gmail.com>
2016-03-03 17:54:51 +0100

    fixes

--- so-lab1/FCFS.h
+++ so-lab1/FCFS.h
@@ -1,4 +1,32 @@
-#include "FCFS.h"
+#pragma once
+
+template <class Item>
+class FCFS
+{
+private:
+	struct Node
+	{
+		Item item;
+		struct Node * next;
+	};
+
+	Node * front;
+	Node * rear;
+	int items;
+public:
+	FCFS();
+	~FCFS();
+	bool isEmpty() const
+	{
+		return items == 0;
+	}
+	int size() const
+	{
+		return items;
+	}
+	bool enqueue(const Item & item);
+	bool dequeue(Item & item);
+};
 
 template <class Item>
 FCFS<Item>::FCFS()

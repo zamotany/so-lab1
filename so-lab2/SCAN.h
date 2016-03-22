4c7bb9a2d8ccbdefd422d5cc120f7383b9182ebf
Kostu96 <kostu96@gmail.com>
2016-03-22 02:28:03 +0100

    SCAN class

--- so-lab2/SCAN.h
+++ so-lab2/SCAN.h
@@ -12,9 +12,6 @@ private:
 	// \var Actual position of the virtual disk head.
 	unsigned int m_headPosition;
 
-	// \var Last position of the virtual disk head.
-	unsigned int m_lastHeadPosition;
-
 	// \var Pointer to the array of next positions.
 	unsigned int * m_array;
 
@@ -30,14 +27,18 @@ public:
 	// \param newPosition New position of the head.
 	void setHeadPosition(unsigned int newPosition);
 
-	// \brief Load an array of positions of the head.
+	// \brief Load an array of positions of the head before scheduling.
 	// \param arr          Pointer to an array.
 	// \param size         Size of the given array.
 	void load(const unsigned int * arr, size_t size);
 
-	// \brief Returns next nearest position of the head.
-	// \return Next nearest position of the head.
-	unsigned int getNext();
+	// \brief Returns an array of the next positions of the head after scheduling.
+	// \return Pointer to an array of the next positions of the head.
+	const unsigned int * get() const;
+
+	// \brief Returns number of elements in the array.
+	// \return Number of elements.
+	size_t size() const { return m_size; }
 
 	// \brief Destructor.
 	~SCAN();
